cc24746eacbdc76b059d08c44f55d5001498e5bd
Yusuf <yusuf861074@gmail.com>
2024-11-19 12:22:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select the navigation menu
+const navbar = document.querySelector('.navbar');
+
+// Add scroll event listener
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('nav-scrolled');
+  } else {
+    navbar.classList.remove('nav-scrolled');
+  }
+});
